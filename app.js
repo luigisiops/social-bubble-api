147b4c60d89c2cfffcc9d6127376cfd1d90be4d1
Lsiopongco <58718096+luigisiops@users.noreply.github.com>
2020-12-01 21:56:34 -0500

    adjusted login route- functional feature

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const { brotliCompressSync } = require('zlib')
 
-//const mysql = require("mysql");
 const cors = require("cors");
-
 const bodyParser = require('body-parser')
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
@@ -39,76 +37,6 @@ app.use(
     })
 );
 
-/*const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "password",
-  database: "LoginSystem",
-});*/
-
-
-
-app.get("/login", (req, res) => {
-    if (req.session.user) {
-        res.send({ loggedIn: true, user: req.session.user });
-    } else {
-        res.send({ loggedIn: false });
-    }
-});
-
-app.get("/login", (req, res) => {
-    if (req.session.user) {
-        res.send({ loggedIn: true, user: req.session.user });
-    } else {
-        res.send({ loggedIn: false });
-    }
-});
-
-app.post('/user', (req, res) => {
-    const firstName = req.body.firstName
-    const lastName = req.body.lastName
-
-
-    let newUser = models.Project.build({
-        first_name: firstName,
-        last_name: lastName
-    })
-
-    newUser.save().then(() => {
-        res.send(console.log('Sent'))
-    })
-})
-
-app.post("/login", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    db.query(
-        "SELECT * FROM users WHERE email = ?;",
-        email,
-        (err, result) => {
-            if (err) {
-                res.send({ err: err });
-            }
-
-            if (result.length > 0) {
-                bcrypt.compare(password, result[0].password, (error, response) => {
-                    if (response) {
-                        req.session.user = result;
-                        console.log(req.session.user);
-                        res.send(result);
-                    } else {
-                        res.send({ message: "Wrong email/password combination!" });
-                    }
-                });
-            } else {
-                res.send({ message: "User doesn't exist" });
-            }
-        }
-    );
-});
-
-
 app.post('/register', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
@@ -116,15 +44,6 @@ app.post('/register', (req, res) => {
         if (err) {
             console.log(err);
         }
-        /*
-            db.query(
-              "INSERT INTO users (email, password) VALUES (?,?)",
-              [email, hash],
-              (err, result) => {
-                console.log(err);
-              }
-            );*/
-
         let newUser = models.User.build({
             email: email,
             password: hash
@@ -138,25 +57,43 @@ app.post('/register', (req, res) => {
     res.send(console.log('registering'))
 });
 
+app.get("/login", (req, res) => {
+    if (req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user });
+    } else {
+        res.send({ loggedIn: false });
+    }
+});
 
 
+app.post("/login", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let user = await models.User.findOne({
+        where: {
+            email: email
+        }
+    })
+    if (user === null) {
+        res.send('error');
+    }
+    if (user != null) {
+        bcrypt.compare(password, user.password, (error, response) => {
+            if (response) {
+                req.session.user = user;
+                console.log(req.session.user);
+                res.send(user);
+            } else {
+                res.send({ message: "Wrong email/password combination!" });
+            }
+        });
+    } else {
+        res.send({ message: "User doesn't exist" });
+    }
+}
+);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
